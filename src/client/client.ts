5346f66730cd6baef7ab249b359b09bd9df0d17c
James Barber <jamesbarber@twinsbaseball.com>
2023-05-24 11:39:44 -0400

    Add a Stats Panel to graphic and Dat GUI

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,5 +1,7 @@
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import Stats from 'three/examples/jsm/libs/stats.module';
+import { GUI } from 'dat.gui';
 
 const scene = new THREE.Scene();
 
@@ -16,7 +18,7 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.addEventListener('change', render); //this line is unnecessary if you are re-rendering within the animation loop
+//controls.addEventListener('change', render)
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
@@ -32,21 +34,36 @@ function onWindowResize() {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  render();
+  //render()
 }
 
-// function animate() {
-//     requestAnimationFrame(animate)
+const stats = new Stats();
+document.body.appendChild(stats.dom);
 
-//     cube.rotation.x += 0.01
-//     cube.rotation.y += 0.01
+const gui = new GUI();
+const cubeFolder = gui.addFolder('Cube');
+cubeFolder.add(cube.rotation, 'x', 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, 'y', 0, Math.PI * 2);
+cubeFolder.add(cube.rotation, 'z', 0, Math.PI * 2);
+cubeFolder.open();
+const cameraFolder = gui.addFolder('Camera');
+cameraFolder.add(camera.position, 'z', 0, 20);
+cameraFolder.open();
 
-//     render()
-// }
+function animate() {
+  requestAnimationFrame(animate);
+
+  //   cube.rotation.x += 0.01;
+  //   cube.rotation.y += 0.01;
+
+  render();
+
+  stats.update();
+}
 
 function render() {
   renderer.render(scene, camera);
 }
 
-//animate()
-render();
+animate();
+//render()
