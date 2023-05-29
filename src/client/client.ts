67a1de5befb9f332e55de25331a9330f61656a30
James Barber <jamesbarber@twinsbaseball.com>
2023-05-29 10:22:49 -0400

    Vector3 Lerp

--- src/client/client.ts
+++ src/client/client.ts
@@ -7330,37 +7330,303 @@
 // ------------------------------------------ //
 // Using tween.js with AnimationMixer Example //
 // ------------------------------------------ //
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+// import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+// import Stats from 'three/examples/jsm/libs/stats.module';
+// import { GUI } from 'dat.gui';
+// import TWEEN from '@tweenjs/tween.js';
+
+// const scene = new THREE.Scene();
+// scene.add(new THREE.AxesHelper(5));
+
+// const light1 = new THREE.SpotLight(); //new THREE.SpotLight();
+// light1.position.set(2.5, 5, 2.5);
+// light1.angle = Math.PI / 8;
+// light1.penumbra = 0.5;
+// light1.castShadow = true;
+// light1.shadow.mapSize.width = 1024;
+// light1.shadow.mapSize.height = 1024;
+// light1.shadow.camera.near = 0.5;
+// light1.shadow.camera.far = 20;
+// scene.add(light1);
+
+// const light2 = new THREE.SpotLight(); //new THREE.SpotLight();
+// light2.position.set(-2.5, 5, 2.5);
+// light2.angle = Math.PI / 8;
+// light2.penumbra = 0.5;
+// light2.castShadow = true;
+// light2.shadow.mapSize.width = 1024;
+// light2.shadow.mapSize.height = 1024;
+// light2.shadow.camera.near = 0.5;
+// light2.shadow.camera.far = 20;
+// scene.add(light2);
+
+// const camera = new THREE.PerspectiveCamera(
+//   75,
+//   window.innerWidth / window.innerHeight,
+//   0.1,
+//   1000
+// );
+// camera.position.set(0.8, 1.4, 1.0);
+
+// const renderer = new THREE.WebGLRenderer();
+// renderer.setSize(window.innerWidth, window.innerHeight);
+// renderer.shadowMap.enabled = true;
+// document.body.appendChild(renderer.domElement);
+
+// const controls = new OrbitControls(camera, renderer.domElement);
+// controls.enableDamping = true;
+// controls.target.set(0, 1, 0);
+
+// const sceneMeshes: THREE.Mesh[] = [];
+
+// const planeGeometry = new THREE.PlaneGeometry(25, 25);
+// const texture = new THREE.TextureLoader().load('img/grid.png');
+// const plane = new THREE.Mesh(
+//   planeGeometry,
+//   new THREE.MeshPhongMaterial({ map: texture })
+// );
+// plane.rotateX(-Math.PI / 2);
+// plane.receiveShadow = true;
+// scene.add(plane);
+// sceneMeshes.push(plane);
+
+// let mixer: THREE.AnimationMixer;
+// let modelReady = false;
+// let modelMesh: THREE.Object3D;
+// const animationActions: THREE.AnimationAction[] = [];
+// let activeAction: THREE.AnimationAction;
+// let lastAction: THREE.AnimationAction;
+// const gltfLoader = new GLTFLoader();
+
+// gltfLoader.load(
+//   'models/ninja.glb',
+//   (gltf) => {
+//     gltf.scene.traverse(function (child) {
+//       if ((child as THREE.Mesh).isMesh) {
+//         let m = child as THREE.Mesh;
+//         m.castShadow = true;
+//         m.frustumCulled = false;
+//       }
+//     });
+
+//     mixer = new THREE.AnimationMixer(gltf.scene);
+
+//     const animationAction = mixer.clipAction((gltf as any).animations[0]);
+//     animationActions.push(animationAction);
+//     animationsFolder.add(animations, 'default');
+//     activeAction = animationActions[0];
+
+//     scene.add(gltf.scene);
+//     modelMesh = gltf.scene;
+
+//     //add an animation from another file
+//     gltfLoader.load(
+//       'models/ninja@baseballSwing.glb',
+//       (gltf) => {
+//         console.log('loaded samba');
+//         const animationAction = mixer.clipAction((gltf as any).animations[0]);
+//         animationActions.push(animationAction);
+//         animationsFolder.add(animations, 'baseballSwing');
+
+//         //add an animation from another file
+//         gltfLoader.load(
+//           'models/ninja@baseballPitch.glb',
+//           (gltf) => {
+//             console.log('loaded baseballPitch');
+//             const animationAction = mixer.clipAction(
+//               (gltf as any).animations[0]
+//             );
+//             animationActions.push(animationAction);
+//             animationsFolder.add(animations, 'baseballPitch');
+
+//             //add an animation from another file
+//             gltfLoader.load(
+//               'models/ninja@mmaKick.glb',
+//               (gltf) => {
+//                 console.log('loaded mmaKick');
+//                 (gltf as any).animations[0].tracks.shift(); //delete the specific track that moves the object forward while running
+//                 const animationAction = mixer.clipAction(
+//                   (gltf as any).animations[0]
+//                 );
+//                 animationActions.push(animationAction);
+//                 animationsFolder.add(animations, 'mmaKick');
+
+//                 modelReady = true;
+//               },
+//               (xhr) => {
+//                 console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+//               },
+//               (error) => {
+//                 console.log(error);
+//               }
+//             );
+//           },
+//           (xhr) => {
+//             console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+//           },
+//           (error) => {
+//             console.log(error);
+//           }
+//         );
+//       },
+//       (xhr) => {
+//         console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+//       },
+//       (error) => {
+//         console.log(error);
+//       }
+//     );
+//   },
+//   (xhr) => {
+//     console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
+//   },
+//   (error) => {
+//     console.log(error);
+//   }
+// );
+
+// window.addEventListener('resize', onWindowResize, false);
+// function onWindowResize() {
+//   camera.aspect = window.innerWidth / window.innerHeight;
+//   camera.updateProjectionMatrix();
+//   renderer.setSize(window.innerWidth, window.innerHeight);
+//   render();
+// }
+
+// const raycaster = new THREE.Raycaster();
+// const targetQuaternion = new THREE.Quaternion();
+
+// renderer.domElement.addEventListener('dblclick', onDoubleClick, false);
+// function onDoubleClick(event: MouseEvent) {
+//   const mouse = {
+//     x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+//     y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1,
+//   };
+//   // @ts-ignore
+//   raycaster.setFromCamera(mouse, camera);
+
+//   const intersects = raycaster.intersectObjects(sceneMeshes, false);
+
+//   if (intersects.length > 0) {
+//     const p = intersects[0].point;
+
+//     const distance = modelMesh.position.distanceTo(p);
+
+//     // modelMesh.lookAt(p);
+
+//     const rotationMatrix = new THREE.Matrix4();
+//     rotationMatrix.lookAt(p, modelMesh.position, modelMesh.up);
+//     targetQuaternion.setFromRotationMatrix(rotationMatrix);
+
+//     setAction(animationActions[2]);
+
+//     TWEEN.removeAll();
+//     new TWEEN.Tween(modelMesh.position)
+//       .to(
+//         {
+//           x: p.x,
+//           y: p.y,
+//           z: p.z,
+//         },
+//         (1000 / 2) * distance
+//       ) //walks 2 meters a second * the distance
+//       .onUpdate(() => {
+//         controls.target.set(
+//           modelMesh.position.x,
+//           modelMesh.position.y + 1,
+//           modelMesh.position.z
+//         );
+//         light1.target = modelMesh;
+//         light2.target = modelMesh;
+//       })
+//       .start()
+//       .onComplete(() => {
+//         setAction(animationActions[1]);
+//         activeAction.clampWhenFinished = true;
+//         activeAction.loop = THREE.LoopOnce;
+//       });
+//   }
+// }
+
+// const stats = new Stats();
+// document.body.appendChild(stats.dom);
+
+// const animations = {
+//   default: function () {
+//     setAction(animationActions[0]);
+//   },
+//   baseballSwing: function () {
+//     setAction(animationActions[1]);
+//   },
+//   baseballPitch: function () {
+//     setAction(animationActions[2]);
+//   },
+//   mmaKick: function () {
+//     setAction(animationActions[3]);
+//   },
+// };
+
+// const setAction = (toAction: THREE.AnimationAction) => {
+//   if (toAction != activeAction) {
+//     lastAction = activeAction;
+//     activeAction = toAction;
+//     //lastAction.stop()
+//     lastAction.fadeOut(0.2);
+//     activeAction.reset();
+//     activeAction.fadeIn(0.2);
+//     activeAction.play();
+//   }
+// };
+
+// const gui = new GUI();
+// const animationsFolder = gui.addFolder('Animations');
+// animationsFolder.open();
+
+// const clock = new THREE.Clock();
+// let delta = 0;
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   controls.update();
+
+//   if (modelReady) {
+//     delta = clock.getDelta();
+//     mixer.update(delta);
+
+//     if (!modelMesh.quaternion.equals(targetQuaternion)) {
+//       modelMesh.quaternion.rotateTowards(targetQuaternion, delta * 1000);
+//     }
+//   }
+
+//   TWEEN.update();
+
+//   render();
+
+//   stats.update();
+// }
+
+// function render() {
+//   renderer.render(scene, camera);
+// }
+
+// animate();
+
+// -------------------- //
+// Vector3 Lerp Example //
+// -------------------- //
+
+// #Might Use
+// This could be much better than tween. Try converting this to use the slider and play button like the ChatGPT example a few up
+// Don't think I'll need lerpVectors, just the regular lerp
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 import Stats from 'three/examples/jsm/libs/stats.module';
 import { GUI } from 'dat.gui';
-import TWEEN from '@tweenjs/tween.js';
 
 const scene = new THREE.Scene();
-scene.add(new THREE.AxesHelper(5));
-
-const light1 = new THREE.SpotLight(); //new THREE.SpotLight();
-light1.position.set(2.5, 5, 2.5);
-light1.angle = Math.PI / 8;
-light1.penumbra = 0.5;
-light1.castShadow = true;
-light1.shadow.mapSize.width = 1024;
-light1.shadow.mapSize.height = 1024;
-light1.shadow.camera.near = 0.5;
-light1.shadow.camera.far = 20;
-scene.add(light1);
-
-const light2 = new THREE.SpotLight(); //new THREE.SpotLight();
-light2.position.set(-2.5, 5, 2.5);
-light2.angle = Math.PI / 8;
-light2.penumbra = 0.5;
-light2.castShadow = true;
-light2.shadow.mapSize.width = 1024;
-light2.shadow.mapSize.height = 1024;
-light2.shadow.camera.near = 0.5;
-light2.shadow.camera.far = 20;
-scene.add(light2);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -7368,124 +7634,37 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.set(0.8, 1.4, 1.0);
+camera.position.set(1, 2, 5);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-renderer.shadowMap.enabled = true;
 document.body.appendChild(renderer.domElement);
 
 const controls = new OrbitControls(camera, renderer.domElement);
-controls.enableDamping = true;
-controls.target.set(0, 1, 0);
 
-const sceneMeshes: THREE.Mesh[] = [];
-
-const planeGeometry = new THREE.PlaneGeometry(25, 25);
-const texture = new THREE.TextureLoader().load('img/grid.png');
-const plane = new THREE.Mesh(
-  planeGeometry,
-  new THREE.MeshPhongMaterial({ map: texture })
+const floor = new THREE.Mesh(
+  new THREE.PlaneGeometry(20, 20, 10, 10),
+  new THREE.MeshBasicMaterial({ color: 0xaec6cf, wireframe: true })
 );
-plane.rotateX(-Math.PI / 2);
-plane.receiveShadow = true;
-scene.add(plane);
-sceneMeshes.push(plane);
+floor.rotateX(-Math.PI / 2);
+scene.add(floor);
 
-let mixer: THREE.AnimationMixer;
-let modelReady = false;
-let modelMesh: THREE.Object3D;
-const animationActions: THREE.AnimationAction[] = [];
-let activeAction: THREE.AnimationAction;
-let lastAction: THREE.AnimationAction;
-const gltfLoader = new GLTFLoader();
-
-gltfLoader.load(
-  'models/ninja.glb',
-  (gltf) => {
-    gltf.scene.traverse(function (child) {
-      if ((child as THREE.Mesh).isMesh) {
-        let m = child as THREE.Mesh;
-        m.castShadow = true;
-        m.frustumCulled = false;
-      }
-    });
-
-    mixer = new THREE.AnimationMixer(gltf.scene);
-
-    const animationAction = mixer.clipAction((gltf as any).animations[0]);
-    animationActions.push(animationAction);
-    animationsFolder.add(animations, 'default');
-    activeAction = animationActions[0];
-
-    scene.add(gltf.scene);
-    modelMesh = gltf.scene;
-
-    //add an animation from another file
-    gltfLoader.load(
-      'models/ninja@baseballSwing.glb',
-      (gltf) => {
-        console.log('loaded samba');
-        const animationAction = mixer.clipAction((gltf as any).animations[0]);
-        animationActions.push(animationAction);
-        animationsFolder.add(animations, 'baseballSwing');
-
-        //add an animation from another file
-        gltfLoader.load(
-          'models/ninja@baseballPitch.glb',
-          (gltf) => {
-            console.log('loaded baseballPitch');
-            const animationAction = mixer.clipAction(
-              (gltf as any).animations[0]
-            );
-            animationActions.push(animationAction);
-            animationsFolder.add(animations, 'baseballPitch');
-
-            //add an animation from another file
-            gltfLoader.load(
-              'models/ninja@mmaKick.glb',
-              (gltf) => {
-                console.log('loaded mmaKick');
-                (gltf as any).animations[0].tracks.shift(); //delete the specific track that moves the object forward while running
-                const animationAction = mixer.clipAction(
-                  (gltf as any).animations[0]
-                );
-                animationActions.push(animationAction);
-                animationsFolder.add(animations, 'mmaKick');
-
-                modelReady = true;
-              },
-              (xhr) => {
-                console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-              },
-              (error) => {
-                console.log(error);
-              }
-            );
-          },
-          (xhr) => {
-            console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-          },
-          (error) => {
-            console.log(error);
-          }
-        );
-      },
-      (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  },
-  (xhr) => {
-    console.log((xhr.loaded / xhr.total) * 100 + '% loaded');
-  },
-  (error) => {
-    console.log(error);
-  }
+const geometry = new THREE.BoxGeometry();
+//the cube used for .lerp
+const cube1 = new THREE.Mesh(
+  geometry,
+  new THREE.MeshBasicMaterial({ color: 0x00ff00, wireframe: true })
 );
+cube1.position.y = 0.5;
+scene.add(cube1);
+
+//the cube used for .lerpVectors
+const cube2 = new THREE.Mesh(
+  geometry,
+  new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+);
+cube2.position.y = 0.5;
+scene.add(cube2);
 
 window.addEventListener('resize', onWindowResize, false);
 function onWindowResize() {
@@ -7496,115 +7675,49 @@ function onWindowResize() {
 }
 
 const raycaster = new THREE.Raycaster();
-const targetQuaternion = new THREE.Quaternion();
+let v1 = new THREE.Vector3(2, 0.5, 2);
+let v2 = new THREE.Vector3(0, 0.5, 0);
+const mouse = new THREE.Vector2();
 
-renderer.domElement.addEventListener('dblclick', onDoubleClick, false);
-function onDoubleClick(event: MouseEvent) {
-  const mouse = {
-    x: (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
-    y: -(event.clientY / renderer.domElement.clientHeight) * 2 + 1,
-  };
-  // @ts-ignore
+function onDoubleClick(event: THREE.Event) {
+  mouse.set(
+    (event.clientX / renderer.domElement.clientWidth) * 2 - 1,
+    -(event.clientY / renderer.domElement.clientHeight) * 2 + 1
+  );
   raycaster.setFromCamera(mouse, camera);
 
-  const intersects = raycaster.intersectObjects(sceneMeshes, false);
-
+  const intersects = raycaster.intersectObject(floor, false);
   if (intersects.length > 0) {
-    const p = intersects[0].point;
-
-    const distance = modelMesh.position.distanceTo(p);
-
-    // modelMesh.lookAt(p);
-
-    const rotationMatrix = new THREE.Matrix4();
-    rotationMatrix.lookAt(p, modelMesh.position, modelMesh.up);
-    targetQuaternion.setFromRotationMatrix(rotationMatrix);
-
-    setAction(animationActions[2]);
-
-    TWEEN.removeAll();
-    new TWEEN.Tween(modelMesh.position)
-      .to(
-        {
-          x: p.x,
-          y: p.y,
-          z: p.z,
-        },
-        (1000 / 2) * distance
-      ) //walks 2 meters a second * the distance
-      .onUpdate(() => {
-        controls.target.set(
-          modelMesh.position.x,
-          modelMesh.position.y + 1,
-          modelMesh.position.z
-        );
-        light1.target = modelMesh;
-        light2.target = modelMesh;
-      })
-      .start()
-      .onComplete(() => {
-        setAction(animationActions[1]);
-        activeAction.clampWhenFinished = true;
-        activeAction.loop = THREE.LoopOnce;
-      });
+    v1 = intersects[0].point;
+    v1.y += 0.5; //raise it so it appears to sit on grid
+    //console.log(v1)
   }
 }
+renderer.domElement.addEventListener('dblclick', onDoubleClick, false);
 
 const stats = new Stats();
 document.body.appendChild(stats.dom);
-
-const animations = {
-  default: function () {
-    setAction(animationActions[0]);
-  },
-  baseballSwing: function () {
-    setAction(animationActions[1]);
-  },
-  baseballPitch: function () {
-    setAction(animationActions[2]);
-  },
-  mmaKick: function () {
-    setAction(animationActions[3]);
-  },
+const data = {
+  lerpAlpha: 0.1,
+  lerpVectorsAlpha: 1.0,
 };
-
-const setAction = (toAction: THREE.AnimationAction) => {
-  if (toAction != activeAction) {
-    lastAction = activeAction;
-    activeAction = toAction;
-    //lastAction.stop()
-    lastAction.fadeOut(0.2);
-    activeAction.reset();
-    activeAction.fadeIn(0.2);
-    activeAction.play();
-  }
-};
-
 const gui = new GUI();
-const animationsFolder = gui.addFolder('Animations');
-animationsFolder.open();
 
-const clock = new THREE.Clock();
-let delta = 0;
+const lerpFolder = gui.addFolder('.lerp');
+lerpFolder.add(data, 'lerpAlpha', 0, 1.0, 0.01);
+lerpFolder.open();
+
+const lerpVectorsFolder = gui.addFolder('.lerpVectors');
+lerpVectorsFolder.add(data, 'lerpVectorsAlpha', 0, 1.0, 0.01);
+lerpVectorsFolder.open();
 
 function animate() {
   requestAnimationFrame(animate);
-
   controls.update();
-
-  if (modelReady) {
-    delta = clock.getDelta();
-    mixer.update(delta);
-
-    if (!modelMesh.quaternion.equals(targetQuaternion)) {
-      modelMesh.quaternion.rotateTowards(targetQuaternion, delta * 1000);
-    }
-  }
-
-  TWEEN.update();
-
+  cube1.position.lerp(v1, data.lerpAlpha);
+  cube2.position.lerpVectors(v1, v2, data.lerpVectorsAlpha);
+  controls.target.copy(cube1.position);
   render();
-
   stats.update();
 }
 
